c8b44a9ffcbe610459308de53cde9830fdaf8d76
XJSWAG <94026153+XJSWAG@users.noreply.github.com>
2022-01-27 07:02:03 -0800

    Update Setup.js

--- Injectables/ModMenu/Setup.js
+++ Injectables/ModMenu/Setup.js
@@ -1,15 +1,7 @@
-if(!blooketUtility.prefs.modmenu) {
-  blooketUtility.prefs.modmenu = {
-    title: "Mod Menu"
-    description: "Mod Menu made of of Glixxzy Scripts"
-    value: {
-      Mod Menu: {
-        title: "Mod Menu",
-        description:
-             "A mod menu with a load of different scripts",
-                        value: false,  
-    },
-  },
- };
-}   
-    
+if (!blooketUtility.prefs.modMenu) {
+  blooketUtility.prefs.modMenu = {
+    title: "Mod Menu",
+    description: "Has a load of scripts that you can use in game",
+    value: true,
+  };
+}
