d4438ef0f99b3df6af614ac1fdfc51598615c921
XJSWAG <94026153+XJSWAG@users.noreply.github.com>
2022-01-26 16:57:39 -0800

    Create Setup.js

---
+++ Injectables/ModMenu/Setup.js
@@ -0,0 +1,20 @@
+if(!blooketUtility.prefs.modmenu) {
+  blooketUtility.prefs.modmenu = {
+    title: "Mod Menu"
+    description: "Mod Menu made of of Glixxzy Scripts"
+    value: {
+      Cafe: {
+        title: "Get Coins",
+        description:
+             "A mod that can give coins for cafe",
+                        value: false,  
+                        title: "Infinite Food Levels", 
+                        description:
+                      "A mod that gives you infinite food level",
+                      value: false,
+                      title: "Stock Infinite Food",
+                     description:
+                    "A mod that stocks an infinite amount of food",
+                      value: false,
+                          },
+    
